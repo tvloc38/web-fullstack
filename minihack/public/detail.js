1abf1f61694154361c27661b9ab9ef4451bcffe6
Stefan Nguyen <nguyenvan.hung26091997@gmail.com>
2018-10-28 21:50:58 +0700

    update

--- minihack/public/detail.js
+++ minihack/public/detail.js
@@ -2,11 +2,11 @@ const params = new URL(window.location.href).pathname.split("/");
 const playerId = params[params.length - 1];
 
 $.ajax({
-    url: "/gamesdetail/" + playerId;
+    url: "/gamesdetail/" + playerId,
     type: "GET",
     success: function(response) {
         if(response && response.success) {
-            console.log(response.player);
+            console.log(response);
         }
     }
 })
