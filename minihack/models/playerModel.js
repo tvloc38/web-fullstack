56a5a3f4279105f44135e73195f243b32b42c84f
Macbook <macbook@Macbooks-MacBook-Pro.local>
2018-10-28 20:37:47 +0700

    playerModel

---
+++ minihack/models/playerModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Model = mongoose.model;
+
+
+const PlayerModel = new Schema({
+    name: { type: String, required: true},
+    creater: { type: Number, required: true},
+    score: { type: Number, default: 0},
+    round: Number,
+});
+
+module.exports = mongoose.model("Player", PlayerSchema);
